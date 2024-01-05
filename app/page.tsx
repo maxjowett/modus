c6f6c7cb5c7ebb19581f66c9a3f71052781e9547
Max Jowett <jowett.max@gmail.com>
2024-01-04 21:52:35 -0800

    Add static data

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+
+import { useState } from "react";
+import { MODES } from "./static/modes";
+
 export default function Home() {
+  const [selectedModeIndex, setSelectedModeIndex] = useState(0);
+
   return (
     <main>
       <div>
-        <h1>MODUS</h1>
+        <h1 className="text-3xl">{MODES[selectedModeIndex].name}</h1>
+        <h1 className="text-1xl">{MODES[selectedModeIndex].description}</h1>
+        <p></p>
       </div>
     </main>
   );
---
+++ app/static/modes.ts
@@ -0,0 +1,15 @@
+type Mode = {
+  name: string;
+  order: number;
+  description: string;
+};
+
+export const MODES: Mode[] = [
+  { name: "Ionian", order: 1, description: "" },
+  { name: "Dorian", order: 2, description: "" },
+  { name: "Phrygian", order: 3, description: "" },
+  { name: "Lydian", order: 4, description: "" },
+  { name: "Mixolydian", order: 5, description: "" },
+  { name: "Aeolian", order: 6, description: "" },
+  { name: "Locrian", order: 7, description: "" },
+];
